7e7a22d0d60f4ab38e82fb1edbaba4e0f6134aab
Hasin Hayder <countdraculla@gmail.com>
2023-07-17 10:46:32 +0600

    Episode 01 Code

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,13 @@
 <script setup>
-
+const items = [1,2,3,4,5]
 </script>
 
 <template>
-  <section class="mx-auto container">
-    <section class="mx-auto container space-y-3">
-        <div class="p-5 border border-gray-600 w-40" v-for="n in 5">{{ n }}</div>
+  <section class="mx-auto container flex items-center text-center flex-col space-y-2">
+        <!-- <div class="p-5 border border-gray-600 w-40" v-for="n in 5"> {{ n }}</div> -->
+        <div class="p-5 border border-gray-600 w-40" v-for="item in items"> {{ item }}</div>
+
     </section>
-  </section>
 </template>
 
 <style scoped>
