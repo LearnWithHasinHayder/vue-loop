e4720c3a02cd98ec94d769b601f6eb4eb55bf8cb
Hasin Hayder <countdraculla@gmail.com>
2023-07-17 09:54:34 +0600

    Episode 01 Code

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+  <section class="mx-auto container">
+    <section class="mx-auto container space-y-3">
+        <div class="p-5 border border-gray-600 w-40" v-for="n in 5">{{ n }}</div>
+    </section>
+  </section>
+</template>
+
+<style scoped>
+
+</style>
