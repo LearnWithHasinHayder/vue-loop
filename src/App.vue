5d1202ab64827bc8505f615ab51e7534aae093f5
Hasin Hayder <countdraculla@gmail.com>
2023-07-17 09:59:04 +0600

    Episode 02 Code

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script setup>
-
+const tasks = [
+  "Complete project proposal",
+  "Update website content",
+  "Fix bug in user authentication",
+  "Prepare presentation slides",
+  "Test new feature implementation",
+];
 </script>
 
 <template>
   <section class="mx-auto container">
     <section class="mx-auto container space-y-3">
-        <div class="p-5 border border-gray-600 w-40" v-for="n in 5">{{ n }}</div>
+        <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks"> {{ index+1 }}. {{ task }}</div>
     </section>
   </section>
 </template>
