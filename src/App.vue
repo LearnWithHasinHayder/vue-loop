5bc32a29a3ddef6a03dc72acec0d312889ed8dfe
Hasin Hayder <countdraculla@gmail.com>
2023-07-17 10:00:18 +0600

    Episode 03 Code

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <script setup>
 const tasks = [
-  "Complete project proposal",
-  "Update website content",
-  "Fix bug in user authentication",
-  "Prepare presentation slides",
-  "Test new feature implementation",
+  { id: 1, title: "Complete project proposal", status: "In Progress" },
+  { id: 2, title: "Update website content", status: "Completed" },
+  { id: 3, title: "Fix bug in user authentication", status: "In Progress" },
+  { id: 4, title: "Prepare presentation slides", status: "Pending" },
+  { id: 5, title: "Test new feature implementation", status: "In Progress" },
 ];
 </script>
 
 <template>
   <section class="mx-auto container">
     <section class="mx-auto container space-y-3">
-        <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks"> {{ index+1 }}. {{ task }}</div>
+        <div class="p-5 border border-gray-600 text-left" v-for="task in tasks" :key="task.id"> {{ task.id }}. {{ task.title }}</div>
     </section>
   </section>
 </template>
