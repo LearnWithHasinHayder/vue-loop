bcaa4ea3f2614e170f05768498462898a4e5367a
Hasin Hayder <countdraculla@gmail.com>
2023-07-17 21:50:10 +0600

    Episode 03 Code

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ const tasks = [
   { id: 2, title: "Update website content", status: "Completed" },
   { id: 3, title: "Fix bug in user authentication", status: "In Progress" },
   { id: 4, title: "Prepare presentation slides", status: "Pending" },
-  { id: 5, title: "Test new feature implementation", status: "Completed" },
+  { id: 5, title: "Test new feature implementation", status: "In Progress" },
 ];
 
 const person = {
@@ -15,12 +15,30 @@ const person = {
 }
 //v,k,i
 
+function getCompleteTasks(){
+  return tasks.filter( task => 'Completed'==task.status)
+}
+
+function getTasksInProgress(){
+  // return tasks.filter( task => 'In Progress'==task.status)
+  return tasks.filter( function(task){
+    return 'In Progress'==task.status
+  })
+}
+
 </script>
 
 <template>
   <section class="mx-auto container">
     <section class="mx-auto container space-y-3">
+      <h2>Completed</h2>
+      <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in getCompleteTasks()" :key="index">{{ task.id }} - {{task.title}} </div>
+      <h2>In Progress</h2>
+      <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in getTasksInProgress()" :key="index">{{ task.id }} - {{task.title}} </div>
+      <h2>Pending</h2>
+      <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks" v-show="'Pending'==task.status" :key="index">{{ task.id }} - {{task.title}} </div>
       <!-- <div class="p-5 border border-gray-600 text-left" v-for="(task, index) in tasks" :key="index">{{ task.id }} - {{task.title}} </div> -->
+      <h2>Object Loop</h2>
       <div class="p-5 border border-gray-600 text-left" v-for="(value, property, index) in person" :key="property">{{ property }} = {{ value }}</div>
 
     </section>
